9ca0b51a42afccc1425746dee582ee63646c5d23
princessval <44687278+valeriaconde@users.noreply.github.com>
2022-05-16 18:11:51 -0500

    tokenization finished

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import requests
 from matplotlib import pyplot as plt
 from matplotlib import cm
 import json
+import re
+
+# Map of tokens
+tokens = {}
 
 query = """query	{
   defectsByReportId(reportId: "6273626676b6958fa71d9315") {
@@ -31,3 +35,17 @@ df_data = jsonData['data']['defectsByReportId']
 df = pandas.DataFrame(df_data)
 print(df)
 
+
+# Tokenization
+tokenizedDF = []
+words = re.findall(r'\w+', df)
+
+for word in words:
+  if not word in tokens:
+    tokens[word] = len(tokens) + 1
+  
+  tokenizedDF.append(tokens[word])
+
+# print tokenized df
+for i in tokenizedDF:
+  print(i)
