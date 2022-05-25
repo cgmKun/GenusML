6e93ce7f3e3c9d822ebea196190fbecc857a512f
princessval <44687278+valeriaconde@users.noreply.github.com>
2022-05-25 11:37:11 -0700

    Tokenizacion modularizada

--- main.py
+++ main.py
@@ -7,8 +7,32 @@ from matplotlib import cm
 import json
 import re
 
-# Map of tokens
+# Map of tokens is global to maintain uniqueness of tokens across multiple calls to method
 tokens = {}
+
+# Receives a string and returns an array of tokens for each word on string
+def tokenize(str):
+  # Output array
+  tokenizedDF = []
+
+  # Lowercases all chars
+  lowerDF = str.lower()
+
+  # Regex hanldes special characters
+  words = re.findall(r'\w+', lowerDF)
+
+  # Tokenizes and appends to output array
+  for word in words:
+    if not word in tokens:
+      tokens[word] = len(tokens) + 1
+  
+    tokenizedDF.append(tokens[word])
+
+  # Print tokenized string
+  for i in tokenizedDF:
+    print(i)
+
+
 
 query = """query	{
   defectsByReportId(reportId: "6273626676b6958fa71d9315") {
@@ -37,19 +61,3 @@ print(df)
     #assignee
     #digitalService
     #summary
-
-# Tokenization
-tokenizedDF = []
-lowerDF = df.lower()
-words = re.findall(r'\w+', lowerDF)
-
-for word in words:
-  if not word in tokens:
-    tokens[word] = len(tokens) + 1
-  
-  tokenizedDF.append(tokens[word])
-
-# print tokenized df
-for i in tokenizedDF:
-  print(i)
-
