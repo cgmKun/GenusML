bbcc0ea6fc92c004653c42072d06001158319452
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-05-26 01:03:04 -0500

    Implement K-means clustering with server data

--- main.py
+++ main.py
@@ -1,55 +1,67 @@
+#Standard Libraries Required
 import pandas
+import sys
 import numpy as np
 import pandas as pd
 import requests
-from matplotlib import pyplot as plt
-from matplotlib import cm
 import json
+
+# SKLEARN LIBRARIES
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+# string manipulation libs
 import re
+import string
+import nltk
+from nltk.corpus import stopwords
 
-# Map of tokens
-tokens = {}
+# viz libs
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-query = """query	{
-  defectsByReportId(reportId: "6273626676b6958fa71d9315") {
-    description
-  }
-}""" 
+#GLOBAL VARIABLES
+reportId = sys.argv[1]
+true_k = sys.argv[2]
 
+def fetchReport(reportId):
+    query = """query    {
+        defectsByReportId(reportId: "%s") {
+            _id
+            description
+        }
+    }"""
 
-url = "http://localhost:8000/api"
-r = requests.post(url, json={'query': query})
-jsonData = json.loads(r.text)
-df_data = jsonData['data']['defectsByReportId']
-print(df_data)
-df = pandas.DataFrame(df_data)
-print(df)
+    url = "http://localhost:8000/api"
+    r = requests.post(url, json={'query': query % (reportId)})
+    jsonData = json.loads(r.text)
+    df_data = jsonData['data']['defectsByReportId']
+    df = pd.DataFrame(df_data)
+    return df
 
+#Fetch Report
+df = fetchReport("626c59cb164957a16a6e2d87")
+print(type(reportId))
 
-#_id
-    #issueKey
-    #status
-    #priority
-    #severity
-    #projectKey
-    #issueType
-    #created
-    #assignee
-    #digitalService
-    #summary
+# STOP WORDS? + VECTORIZER
+vectorizer = TfidfVectorizer(stop_words={'english', 'spanish'})
+X = vectorizer.fit_transform(df.description)
 
-# Tokenization
-tokenizedDF = []
-lowerDF = df.lower()
-words = re.findall(r'\w+', lowerDF)
+# Sum_of_squared_distances = []
+# K = range(2,10)
+# for k in K:
+#     km = KMeans(n_clusters=k, max_iter=200, n_init=10)
+#     km = km.fit(X)
+#     Sum_of_squared_distances.append(km.inertia_)
 
-for word in words:
-  if not word in tokens:
-    tokens[word] = len(tokens) + 1
-  
-  tokenizedDF.append(tokens[word])
+model = KMeans(n_clusters = int(true_k), init = 'k-means++', max_iter = 200, n_init = 10)
+model.fit(X)
+labels = model.labels_
+clustered_defects = pd.DataFrame(list(zip(df._id, df.description, labels)),columns=['_id', 'description','group'])
 
-# print tokenized df
-for i in tokenizedDF:
-  print(i)
-
+for k in range(0,int(true_k)):
+    print("cluster")
+    mask = clustered_defects['group'] == k
+    test = clustered_defects[mask]
