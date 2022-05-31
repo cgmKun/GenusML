dda56a6ad7c6cc31cf6771dbb63b67014e6ed353
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-05-31 08:40:25 -0500

    Submit Groups to the database

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 #Standard Libraries
-import pandas
 import sys
-import numpy as np
 import pandas as pd
 import requests
 import json
 import re
 
 # Sklearn
-from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
 
 # Plotting Libraries
 import matplotlib.pyplot as plt
@@ -19,24 +15,59 @@ import seaborn as sns
 
 #Global Variables - Command Line Arguments
 reportId = sys.argv[1]
-true_k = sys.argv[2]
+sessionId = sys.argv[2]
+true_k = sys.argv[3]
 
 # Fetch defects by reportID GraphQL query
 # Returns a dataframe with the query results
-def fetchReport(reportId):
-    query = """query    {
-        defectsByReportId(reportId: "%s") {
-            _id
-            description
-        }
-    }"""
+def fetchReport():
+  query = """
+    query {{
+      defectsByReportId(reportId: "{reportId}") {{
+        _id
+        description
+      }}
+    }}    
+  """.format(reportId = reportId)
 
-    url = "http://localhost:8000/api"
-    r = requests.post(url, json={'query': query % (reportId)})
-    jsonData = json.loads(r.text)
-    df_data = jsonData['data']['defectsByReportId']
-    df = pd.DataFrame(df_data)
-    return df
+  url = "http://localhost:8000/api"
+  r = requests.post(url, json={'query': query})
+  jsonData = json.loads(r.text)
+  df_data = jsonData['data']['defectsByReportId']
+  df = pd.DataFrame(df_data)
+  return df
+
+def submitGroup(group_df, group_key):
+  groupTitle = "Group " + str(group_key+1)
+  ids = group_df['_id']
+  defect_ids = ""
+
+  for i in range(len(ids)):
+    defect_ids+=""""{defectId}", """.format(defectId = ids.iloc[i])
+
+  query = """
+    mutation {{
+      createGroup(groupInput: {{
+        groupTitle: "{groupTitle}"
+        sessionId: "{sessionId}"
+        submitDate: "{submitDate}"
+        defects: [{defectIds}]
+        linkedReport: "{reportId}"
+      }}) {{
+        _id
+      }}
+    }}
+  """.format(
+    groupTitle = groupTitle,
+    sessionId = sessionId,
+    submitDate = "hoy",
+    defectIds = defect_ids,
+    reportId = reportId
+  )
+
+  url = "http://localhost:8000/api"
+  r = requests.post(url, json={'query': query})
+  print(r)
 
 # Map of tokens is global to maintain uniqueness of tokens across multiple calls to method
 tokens = {}
@@ -64,25 +95,28 @@ def tokenize(str):
     print(i)
 
 def main():
-    #Fetch Report data
-    defects = fetchReport(reportId)
+  #Fetch Report data
+  defects = fetchReport()
 
-    #Vectorize the description from the defects dataframe
-    vectorizer = TfidfVectorizer(stop_words={'english', 'spanish'})
-    vectorized_defects_description = vectorizer.fit_transform(defects.description)
+  #Vectorize the description from the defects dataframe
+  vectorizer = TfidfVectorizer(stop_words={'english', 'spanish'})
+  vectorized_defects_description = vectorizer.fit_transform(defects.description)
 
-    # Setup and run K-Means Clustering Model - Unsupervised Model
-    model = KMeans(n_clusters = int(true_k), init = 'k-means++', max_iter = 200, n_init = 10)
-    model.fit(vectorized_defects_description)
-    labels = model.labels_
+  # Setup and run K-Means Clustering Model - Unsupervised Model
+  model = KMeans(n_clusters = int(true_k), init = 'k-means++', max_iter = 200, n_init = 10)
+  model.fit(vectorized_defects_description)
+  labels = model.labels_
 
-    # Retrieve the clustered defects in a dataframe
-    clustered_defects = pd.DataFrame(list(zip(defects._id, defects.description, labels)),columns=['_id', 'description','group'])
+  # Retrieve the clustered defects in a dataframe
+  clustered_defects = pd.DataFrame(list(zip(defects._id, defects.description, labels)),columns=['_id', 'description','group'])
 
-    # Split each cluster into individual dataframes
-    for k in range(0,int(true_k)):
-        mask = clustered_defects['group'] == k
-        test = clustered_defects[mask]
-        print(test)
+  # Split each cluster into individual dataframes
+  for k in range(0,int(true_k)):
+    new_df = clustered_defects.loc[(clustered_defects['group'] == k)]
+    submitGroup(new_df, k)
 
 main()
+
+#https://medium.com/mlearning-ai/text-clustering-with-tf-idf-in-python-c94cd26a31e7
+#https://towardsdatascience.com/clustering-documents-with-python-97314ad6a78d
+#https://github.com/dpanagop/ML_and_AI_examples/blob/master/NLP_example_clustering.ipynb
